e33c105459b833f7aca1b84a643f39ca643c13ed
Crowdbotics <team@crowdbotics.com>
2021-07-07 23:32:41 +0000

    Update models

--- backend/ca_billing_28567/wsgi.py
+++ backend/ca_billing_28567/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ca_billing_28567.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ca_billing_28567.settings")
 
 application = get_wsgi_application()
